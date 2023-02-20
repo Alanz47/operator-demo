045c540c591abeca98db51151048fd8a467c522e
Alanz47 <363353028@qq.com>
2023-02-20 15:57:41 +0800

    define crd

--- api/v1/app_types.go
+++ api/v1/app_types.go
@@ -28,8 +28,11 @@ type AppSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of App. Edit app_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Action to some object
+	Action string `json:"action"`
+
+	// Object what action to
+	Object string `json:"object"`
 }
 
 // AppStatus defines the observed state of App
